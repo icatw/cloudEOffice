cc94b21d905c1b2014e57f9859d522966c66f129
icatw <762188827@qq.com>
2022-05-10 09:26:22 +0800

    Initial commit

---
+++ yebSpringBoot/yeb-server/src/main/java/cn/icatw/yeb/server/YebApplication.java
@@ -0,0 +1,19 @@
+package cn.icatw.yeb.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author icatw
+ * @date 2022/5/10
+ * @email 762188827@qq.com
+ * @apiNote
+ */
+@SpringBootApplication
+@MapperScan("cn.icatw.yeb.server.mapper")
+public class YebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YebApplication.class, args);
+    }
+}
