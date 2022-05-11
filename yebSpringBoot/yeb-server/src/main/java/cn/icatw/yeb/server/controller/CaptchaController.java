8ffe67e6b4562f2b9b5f8c4335ffacb1212943f6
icatw <762188827@qq.com>
2022-05-11 16:00:27 +0800

    集成谷歌kaptcha完成验证码接口

---
+++ yebSpringBoot/yeb-server/src/main/java/cn/icatw/yeb/server/controller/CaptchaController.java
@@ -0,0 +1,69 @@
+package cn.icatw.yeb.server.controller;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @author icatw
+ * @date 2022/5/11
+ * @email 762188827@qq.com
+ * @apiNote
+ */
+@Slf4j
+@RestController
+public class CaptchaController {
+    @Autowired
+    private DefaultKaptcha defaultKaptcha;
+
+    @ApiOperation(value = "验证码")
+    @GetMapping(value = "/captcha",produces = "image/jpeg")
+    public void captcha(HttpServletRequest request, HttpServletResponse response) {
+        // 定义response输出类型为image/jpeg类型
+        response.setDateHeader("Expires", 0);
+        // Set standard HTTP/1.1 no-cache headers.
+        response.setHeader("Cache-Control", "no-store, no-cache, mustrevalidate");
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        // Set standard HTTP/1.0 no-cache header.
+        response.setHeader("Pragma", "no-cache");
+        // return a jpeg
+        response.setContentType("image/jpeg");
+        //---------------生成验证码begin
+        //获取验证码文本内容
+        String text = defaultKaptcha.createText();
+        log.info("验证码内容为：" + text);
+        //将验证码文本内容放入session，//TODO 可优化为存入redis
+        request.getSession().setAttribute("captcha", text);
+        //根据文本验证码内容创建图形验证码
+        BufferedImage image = defaultKaptcha.createImage(text);
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            //输出流输出图片，格式为jpg
+            ImageIO.write(image, "jpg", outputStream);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //---------------生成验证码end
+    }
+}
