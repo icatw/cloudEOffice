dc8667ee78f48445542e50ac195312ea94f9acf3
icatw <762188827@qq.com>
2022-05-15 21:48:10 +0800

    处理菜单 引入vuex

--- yeb/src/main.js
+++ yeb/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import store from "./store";
 import {postRequest} from "./utils/api";
 import {putRequest} from "./utils/api";
 import {getRequest} from "./utils/api";
@@ -13,11 +13,12 @@ import {deleteRequest} from "./utils/api";
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 //插件形式发送请求
-Vue.prototype.postRequest=postRequest;
-Vue.prototype.getRequest=getRequest;
-Vue.prototype.deleteRequest=deleteRequest;
-Vue.prototype.putRequest=putRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.putRequest = putRequest;
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app')
---
+++ yeb/src/views/Test2.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  测试2
+</div>
+</template>
+
+<script>
+export default {
+  name: "Test2"
+}
+</script>
+
+<style scoped>
+
+</style>
